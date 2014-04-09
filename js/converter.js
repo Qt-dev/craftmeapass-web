6aeab20fc09cf07ace0ecfd8413bec44d23c0e16
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-08 23:56:42 -0700

    Add the text attribute to the password object

--- js/converter.js
+++ js/converter.js
@@ -1,6 +1,8 @@
 Converter = (function(){
 
-  var Password = function(params){}
+  var Password = function(params){
+    this.text = '';
+  }
 
   function createPassword(params){
     return new Password(params);
--- spec/js/ConverterSpec.js
+++ spec/js/ConverterSpec.js
@@ -2,4 +2,16 @@ describe('Converter', function(){
   it('should exist', function(){
     expect(Converter).toBeDefined();
   })
+
+  describe('the returned password', function(){
+    it('should be an object', function(){
+      var type = typeof Converter();
+      expect(type).toEqual('object')
+    })
+    it('should have a text attribute that is a string', function(){
+      var type = typeof Converter().text;
+      expect(type).toEqual('string')
+    })
+
+  })
 })
